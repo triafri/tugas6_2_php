8ff6ade53d8f28cc1864834e5ffe4f1a480a89f4
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-10-08 10:47:26 +0700

    Add files via upload

---
+++ tugas6_2.php
@@ -0,0 +1,14 @@
+<?php
+	$jambu=15000;
+	$dus=2000;
+	$jumDus = 6;
+	$kilo=30;
+	$total = ($jambu*$kilo)+($dus*$jumDus);
+
+	echo "=========================================== <br>";
+	echo "Harga Jambu = Rp $jambu / kg <br>";
+	echo "Harga Kardus Bekas = Rp 2000 <br>";
+	echo "Total Penjualan (Dus dan Jambu) = Rp $total<br>";
+	echo "=========================================== ";
+
+?>
